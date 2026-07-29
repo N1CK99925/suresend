03a29a4cd83afbe80d0b2637bc3b66030380ad0b
Shailja <S24CSEU1527@bennett.edu.in>
2026-07-29 17:57:33 +0530

    feat: add frontend entry point and responsive styling

---
+++ frontend/src/main.jsx
@@ -0,0 +1,15 @@
+globalThis.global = globalThis;
+
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import { initAnalytics } from "./lib/analytics.js";
+
+initAnalytics();
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
